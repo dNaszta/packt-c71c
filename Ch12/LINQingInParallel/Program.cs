88af705f7dca006f453a305f0490238f36f20108
dNaszta <david.nasztanovics@stylersonline.com>
2019-01-07 16:59:36 +0100

    use Linq for parallel query

---
+++ Ch12/LINQingInParallel/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+
+namespace LINQingInParallel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watch = Stopwatch.StartNew();
+            Write("Press ENTER to start: ");
+            ReadLine();
+            watch.Start();
+            IEnumerable<int> numbers = Enumerable.Range(1, 200_000_000);
+            var squares = numbers.AsParallel().Select(number => number * 2).ToArray();
+            watch.Stop();
+            WriteLine($"{watch.ElapsedMilliseconds:#,##0} elapsed milliseconds.");
+        }
+    }
+}
