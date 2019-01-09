cc52f79112fd247c6fc6cb3d87f4a3e7d179aec3
dNaszta <david.nasztanovics@stylersonline.com>
2019-01-09 10:29:37 +0100

    start northwind entity structure

---
+++ Part3/NorthwindEntitiesLib/Shipper.cs
@@ -0,0 +1,12 @@
+namespace NorthwindEntitiesLib
+{
+    using System.Collections.Generic;
+
+    public class Shipper
+    {
+        public int ShipperID { get; set; }
+        public string ShipperName { get; set; }
+        public string Phone { get; set; }
+        public ICollection<Order> Orders { get; set; }
+    }
+}
